97ce32b3d52a5b8d0790e84439666ea54d2fa715
Alex <slowdsports@gmail.com>
2021-08-08 15:53:39 -0600

    Update base.js

--- mlb/base.js
+++ mlb/base.js
@@ -1,6 +1,9 @@
 let ml0 = "http://g.givemenbastreams.com/hls/";
 let ml1 = "/playlist.m3u8";
 let rays = ml0+ "rays" + ml1;
+let orioles = ml0+ "orioles" + ml1;
+let mets = ml0+ "mets" + ml1;
+let phillies = ml0+ "phillies" + ml1;
 
 function getParameterByName(name) {
                       name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
@@ -14,3 +17,6 @@ function getParameterByName(name) {
                 if (srcLOAD == "") {alert('El canal se encuentra Offline');}
 
                 if (srcLOAD == "rays") {srcLOAD = rays;}
+                if (srcLOAD == "orioles") {srcLOAD = orioles;}
+                if (srcLOAD == "mets") {srcLOAD = mets;}
+                if (srcLOAD == "phillies") {srcLOAD = phillies;}
