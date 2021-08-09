e3f2eb350458893736a76e61d11d416a437ff69b
Alex <slowdsports@gmail.com>
2021-08-09 15:57:03 -0600

    asd

--- mlb/base.js
+++ mlb/base.js
@@ -1,35 +1,70 @@
+window.onload = function() {
+             var player = new Clappr.Player({
+                 source: srcLOAD,
+                 parentId: '#player',
+                 mimeType: 'audio/mpeg',
+                 watermark: "https://i.ibb.co/Tgk2YrC/Logow-1-1.png", 
+                 position: 'top-left',
+                 plugins: [LevelSelector, ChromecastPlugin, ClapprPip.PipButton, ClapprPip.PipPlugin, DashShakaPlayback, Clappr.MediaControl, PlaybackRatePlugin],
+                 events: {onReady: function() {var plugin = this.getPlugin('click_to_pause'); plugin && plugin.disable();},},
+                 height: '100%',
+                 width: '100%',
+                 autoPlay: false,
+                 mute: true,
+                 shakaConfiguration: {
+                 manifest: {retryParameters: {maxAttempts: Infinity}},
+                 streaming: {retryParameters: {maxAttempts: Infinity}},
+                 drm: {retryParameters: {maxAttempts: Infinity}},
+                 },
+                 playbackRateConfig: {
+                 defaultValue: '1.00x',
+                 options: [
+                 {value: '0.10', label: '0.10x'},
+                 {value: '0.25', label: '0.25x'},
+                 {value: '0.50', label: '0.50x'},
+                 {value: '0.75', label: '0.75x'},
+                 {value: '1.00', label: '1.00x'},
+                 {value: '1.25', label: '1.25x'},
+                 {value: '1.50', label: '1.50x'},
+                 {value: '1.75', label: '1.75x'},
+                 {value: '2.00', label: '2.00x'},
+                 ]
+                 },
+             });
+         };
+
 let ml0 = "http://g.givemenbastreams.com/hls/";
 let ml1 = "/playlist.m3u8";
-//let CHathletics = ml0+"athletics"+ml1;
-//let CHtwins = ml0+"twins"+ml1;
-//let CHrays = ml0+"rays"+ml1;
-//let CHyankees = ml0+"yankees"+ml1;
-//let CHwhitesox = ml0+"whitesox"+ml1;
-let CHindians = ml0+"indians"+ml1;
-//let CHastros = ml0+"astros"+ml1;
-//let CHbluejays = ml0+"bluejays"+ml1;
-//let CHorioles = ml0+"orioles"+ml1;
-//let CHtigers = ml0+"tigers"+ml1;
-//let CHroyals = ml0+"royals"+ml1;
-//let CHmariners = ml0+"mariners"+ml1;
-//let CHrangers = ml0+"rangers"+ml1;
-//let CHredsox = ml0+"redsox"+ml1;
-//let CHangels = ml0+"angels"+ml1;
-//let CHdodgers = ml0+"dodgers"+ml1;
-//let CHcubs = ml0+"cubs"+ml1;
-//let CHpadres = ml0+"padres"+ml1;
-//let CHbraves = ml0+"braves"+ml1;
-//let CHmarlins = ml0+"marlins"+ml1;
-//let CHcardinals = ml0+"cardinals"+ml1;
-//let CHrockies = ml0+"rockies"+ml1;
-//let CHgiants = ml0+"giants"+ml1;
-//let CHbrewers = ml0+"brewers"+ml1;
-//let CHphillies = ml0+"phillies"+ml1;
-//let CHmets = ml0+"mets"+ml1;
-//let CHdiamondbacks = ml0+"diamondbacks"+ml1;
-//let CHnationals = ml0+"nationals"+ml1;
-let CHreds = ml0+"reds"+ml1;
-//let CHpirates = ml0+"pirates"+ml1;
+let athletics = ml0+"athletics"+ml1;
+let twins = ml0+"twins"+ml1;
+let rays = ml0+"rays"+ml1;
+let yankees = ml0+"yankees"+ml1;
+let whitesox = ml0+"whitesox"+ml1;
+let indians = ml0+"indians"+ml1;
+let astros = ml0+"astros"+ml1;
+let bluejays = ml0+"bluejays"+ml1;
+let orioles = ml0+"orioles"+ml1;
+let tigers = ml0+"tigers"+ml1;
+let royals = ml0+"royals"+ml1;
+let mariners = ml0+"mariners"+ml1;
+let rangers = ml0+"rangers"+ml1;
+let redsox = ml0+"redsox"+ml1;
+let angels = ml0+"angels"+ml1;
+let dodgers = ml0+"dodgers"+ml1;
+let cubs = ml0+"cubs"+ml1;
+let padres = ml0+"padres"+ml1;
+let braves = ml0+"braves"+ml1;
+let marlins = ml0+"marlins"+ml1;
+let cardinals = ml0+"cardinals"+ml1;
+let rockies = ml0+"rockies"+ml1;
+let giants = ml0+"giants"+ml1;
+let brewers = ml0+"brewers"+ml1;
+let phillies = ml0+"phillies"+ml1;
+let mets = ml0+"mets"+ml1;
+let diamondbacks = ml0+"diamondbacks"+ml1;
+let nationals = ml0+"nationals"+ml1;
+let reds = ml0+"reds"+ml1;
+let pirates = ml0+"pirates"+ml1;
 
 function getParameterByName(name) {
                       name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
@@ -42,33 +77,33 @@ function getParameterByName(name) {
                 if (srcLOAD == "#") {alert('El canal se encuentra Offline');}
                 if (srcLOAD == "") {alert('El canal se encuentra Offline');}
 
-//                if (srcLOAD == "athletics") {srcLOAD = CHathletics;}
-//                if (srcLOAD == "twins") {srcLOAD = CHtwins;}
-//                if (srcLOAD == "rays") {srcLOAD = CHrays;}
-//                if (srcLOAD == "yankees") {srcLOAD = CHyankees;}
-//                if (srcLOAD == "whitesox") {srcLOAD = CHwhitesox;}
-                if (srcLOAD == "indians") {srcLOAD = CHindians;}
-//                if (srcLOAD == "astros") {srcLOAD = CHastros;}
-//                if (srcLOAD == "bluejays") {srcLOAD = CHbluejays;}
-//                if (srcLOAD == "orioles") {srcLOAD = CHorioles;}
-//                if (srcLOAD == "tigers") {srcLOAD = CHtigers;}
-//                if (srcLOAD == "royals") {srcLOAD = CHroyals;}
-//                if (srcLOAD == "mariners") {srcLOAD = CHmariners;}
-//                if (srcLOAD == "rangers") {srcLOAD = CHrangers;}
-//                if (srcLOAD == "redsox") {srcLOAD = CHredsox;}
-//                if (srcLOAD == "angels") {srcLOAD = CHangels;}
-//                if (srcLOAD == "dodgers") {srcLOAD = CHdodgers;}
-//                if (srcLOAD == "cubs") {srcLOAD = CHcubs;}
-//                if (srcLOAD == "padres") {srcLOAD = CHpadres;}
-//                if (srcLOAD == "braves") {srcLOAD = CHbraves;}
-//                if (srcLOAD == "marlins") {srcLOAD = CHmarlins;}
-//                if (srcLOAD == "cardinals") {srcLOAD = CHcardinals;}
-//                if (srcLOAD == "rockies") {srcLOAD = CHrockies;}
-//                if (srcLOAD == "giants") {srcLOAD = CHgiants;}
-//                if (srcLOAD == "brewers") {srcLOAD = CHbrewers;}
-//                if (srcLOAD == "phillies") {srcLOAD = CHphillies;}
-//                if (srcLOAD == "mets") {srcLOAD = CHmets;}
-//                if (srcLOAD == "diamondbacks") {srcLOAD = CHdiamondbacks;}
-//                if (srcLOAD == "nationals") {srcLOAD = CHnationals;}
-//                if (srcLOAD == "reds") {srcLOAD = CHreds;}
-//                if (srcLOAD == "pirates") {srcLOAD = CHpirates;}
+                if (srcLOAD == "athletics") {srcLOAD = athletics;}
+                if (srcLOAD == "twins") {srcLOAD = twins;}
+                if (srcLOAD == "rays") {srcLOAD = rays;}
+                if (srcLOAD == "yankees") {srcLOAD = yankees;}
+                if (srcLOAD == "whitesox") {srcLOAD = whitesox;}
+                if (srcLOAD == "indians") {srcLOAD = indians;}
+                if (srcLOAD == "astros") {srcLOAD = astros;}
+                if (srcLOAD == "bluejays") {srcLOAD = bluejays;}
+                if (srcLOAD == "orioles") {srcLOAD = orioles;}
+                if (srcLOAD == "tigers") {srcLOAD = tigers;}
+                if (srcLOAD == "royals") {srcLOAD = royals;}
+                if (srcLOAD == "mariners") {srcLOAD = mariners;}
+                if (srcLOAD == "rangers") {srcLOAD = rangers;}
+                if (srcLOAD == "redsox") {srcLOAD = redsox;}
+                if (srcLOAD == "angels") {srcLOAD = angels;}
+                if (srcLOAD == "dodgers") {srcLOAD = dodgers;}
+                if (srcLOAD == "cubs") {srcLOAD = cubs;}
+                if (srcLOAD == "padres") {srcLOAD = padres;}
+                if (srcLOAD == "braves") {srcLOAD = braves;}
+                if (srcLOAD == "marlins") {srcLOAD = marlins;}
+                if (srcLOAD == "cardinals") {srcLOAD = cardinals;}
+                if (srcLOAD == "rockies") {srcLOAD = rockies;}
+                if (srcLOAD == "giants") {srcLOAD = giants;}
+                if (srcLOAD == "brewers") {srcLOAD = brewers;}
+                if (srcLOAD == "phillies") {srcLOAD = phillies;}
+                if (srcLOAD == "mets") {srcLOAD = mets;}
+                if (srcLOAD == "diamondbacks") {srcLOAD = diamondbacks;}
+                if (srcLOAD == "nationals") {srcLOAD = nationals;}
+                if (srcLOAD == "reds") {srcLOAD = reds;}
+                if (srcLOAD == "pirates") {srcLOAD = pirates;}
