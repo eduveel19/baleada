0892b03a41360a3cabc3bb2edc7741eccacaef84
Alex <slowdsports@gmail.com>
2021-08-08 22:02:25 -0600

    Create canales.js

---
+++ mlb/canales.js
@@ -0,0 +1,239 @@
+let giants = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=giants">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - giants | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("giants")){ele.innerHTML=(giants)};
+
+let rays = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=rays">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - rays | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("rays")){ele.innerHTML=(rays)};
+
+let dodgers = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=dodgers">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - dodgers | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("dodgers")){ele.innerHTML=(dodgers)};
+
+let whitesox = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=whitesox">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - white sox | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("whitesox")){ele.innerHTML=(whitesox)};
+
+let astros = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=astros">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - astros | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("astros")){ele.innerHTML=(astros)};
+
+let brewers = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=brewers">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - brewers | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("brewers")){ele.innerHTML=(brewers)};
+
+let athletics = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=athletics">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - athletics | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("athletics")){ele.innerHTML=(athletics)};
+
+let redsox = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=redsox">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - red sox | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("redsox")){ele.innerHTML=(redsox)};
+
+let padres = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=padres">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - padres | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("padres")){ele.innerHTML=(padres)};
+
+let yankees = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=yankees">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - yankees | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("yankees")){ele.innerHTML=(yankees)};
+
+let reds = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=reds">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - reds | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("reds")){ele.innerHTML=(reds)};
+
+let bluejays = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=bluejays">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - blue jays | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("bluejays")){ele.innerHTML=(bluejays)};
+
+let phillies = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=phillies">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - phillies | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("phillies")){ele.innerHTML=(phillies)};
+
+let mariners = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=mariners">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - mariners | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("mariners")){ele.innerHTML=(mariners)};
+
+let braves = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=braves">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - braves | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("braves")){ele.innerHTML=(braves)};
+
+let mets = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=mets">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - mets | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("mets")){ele.innerHTML=(mets)};
+
+let angels = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=angels">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - angels | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("angels")){ele.innerHTML=(angels)};
+
+let cardinals = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=cardinals">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - cardinals | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("cardinals")){ele.innerHTML=(cardinals)};
+
+let indians = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=indians">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - indians | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("indians")){ele.innerHTML=(indians)};
+
+let tigers = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=tigers">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - tigers | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("tigers")){ele.innerHTML=(tigers)};
+
+let cubs = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=cubs">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - cubs | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("cubs")){ele.innerHTML=(cubs)};
+
+let rockies = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=rockies">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - rockies | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("rockies")){ele.innerHTML=(rockies)};
+
+let nationals = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=nationals">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - nationals | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nationals")){ele.innerHTML=(nationals)};
+
+let royals = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=royals">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - royals | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("royals")){ele.innerHTML=(royals)};
+
+let twins = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=twins">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - twins | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("twins")){ele.innerHTML=(twins)};
+
+let marlins = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=marlins">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - marlins | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("marlins")){ele.innerHTML=(marlins)};
+
+let pirates = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=pirates">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - pirates | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("pirates")){ele.innerHTML=(pirates)};
+
+let rangers = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=rangers">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - rangers | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("rangers")){ele.innerHTML=(rangers)};
+
+let orioles = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=orioles">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - orioles | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("orioles")){ele.innerHTML=(orioles)};
+
+let diamondbacks = `
+<a href="http://slowdsports.000webhostapp.com/mlb.html?src=diamondbacks">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> MLB - diamondbacks | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("diamondbacks")){ele.innerHTML=(diamondbacks)};
