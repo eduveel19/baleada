3e8eb3ea6a5db3c1bd9fae0aec2f9d2fbd0dd2dc
Alex <slowdsports@gmail.com>
2021-08-10 15:26:04 -0600

    Create canales.js

---
+++ nba/canales.js
@@ -0,0 +1,48 @@
+let NBA1 = `
+<a href="http://slowdsports.000webhostapp.com/nba.html?src=nba1">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> NBA (1) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-1")){ele.innerHTML=(NBA1)};
+
+let NBA2 = `
+<a href="http://slowdsports.000webhostapp.com/nba.html?src=nba2">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> NBA (2) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-2")){ele.innerHTML=(NBA2)};
+
+let NBA3 = `
+<a href="http://slowdsports.000webhostapp.com/nba.html?src=nba3">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> NBA (3) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-3")){ele.innerHTML=(NBA3)};
+
+let NBA4 = `
+<a href="http://slowdsports.000webhostapp.com/nba.html?src=nba4">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> NBA (4) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-4")){ele.innerHTML=(NBA4)};
+
+let NBA5 = `
+<a href="http://slowdsports.000webhostapp.com/nba.html?src=nba5">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> NBA (5) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-5")){ele.innerHTML=(NBA5)};
+
+let NBA6 = `
+<a href="http://slowdsports.000webhostapp.com/nba.html?src=nba6">
+<li class="canalito list-group-item darkolorbg blancolor">
+<i class="us flag"></i> NBA (6) | HD</li>
+</a>
+`;
+for(const ele of document.getElementsByClassName("nba-6")){ele.innerHTML=(NBA6)};
+
