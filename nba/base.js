a5e45ebf439d7e2af2fc25019a93ec3ba41813cc
Alex <slowdsports@gmail.com>
2021-08-10 15:27:48 -0600

    Update base.js

--- nba/base.js
+++ nba/base.js
@@ -1,9 +1,12 @@
 let ml0 = "http://g.givemenbastreams.com/hls/";
 let ml1 = "/playlist.m3u8";
-let rays = ml0+ "rays" + ml1;
-let orioles = ml0+ "orioles" + ml1;
-let mets = ml0+ "mets" + ml1;
-let phillies = ml0+ "phillies" + ml1;
+
+let nba1 = "link.m3u8";
+let nba2 = "";
+let nba3 = "";
+let nba4 = "";
+let nba5 = "";
+let nba6 = "";
 
 function getParameterByName(name) {
                       name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
@@ -16,7 +19,9 @@ function getParameterByName(name) {
                 if (srcLOAD == "#") {alert('El canal se encuentra Offline');}
                 if (srcLOAD == "") {alert('El canal se encuentra Offline');}
 
-                if (srcLOAD == "rays") {srcLOAD = rays;}
-                if (srcLOAD == "orioles") {srcLOAD = orioles;}
-                if (srcLOAD == "mets") {srcLOAD = mets;}
-                if (srcLOAD == "phillies") {srcLOAD = phillies;}
+                if (srcLOAD == "nba1") {srcLOAD = nba1;}
+                if (srcLOAD == "nba2") {srcLOAD = nba2;}
+                if (srcLOAD == "nba3") {srcLOAD = nba3;}
+                if (srcLOAD == "nba4") {srcLOAD = nba4;}
+                if (srcLOAD == "nba5") {srcLOAD = nba5;}
+                if (srcLOAD == "nba6") {srcLOAD = nba6;}
