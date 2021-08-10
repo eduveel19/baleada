31fde12c0edafa97779ff87a5230392aba16ea86
Alex <slowdsports@gmail.com>
2021-08-09 18:07:32 -0600

    Update canales.js

--- mlb/canales.js
+++ mlb/canales.js
@@ -1,7 +1,7 @@
 let MLBgiants = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=giants">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - giants | HD</li>
+<i class="us flag"></i> MLB TV - giants | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-giants")){ele.innerHTML=(MLBgiants)};
@@ -9,7 +9,7 @@ for(const ele of document.getElementsByClassName("mltv-giants")){ele.innerHTML=(
 let MLBrays = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=rays">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - rays | HD</li>
+<i class="us flag"></i> MLB TV - rays | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-rays")){ele.innerHTML=(MLBrays)};
@@ -17,7 +17,7 @@ for(const ele of document.getElementsByClassName("mltv-rays")){ele.innerHTML=(ML
 let MLBdodgers = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=dodgers">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - dodgers | HD</li>
+<i class="us flag"></i> MLB TV - dodgers | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-dodgers")){ele.innerHTML=(MLBdodgers)};
@@ -25,7 +25,7 @@ for(const ele of document.getElementsByClassName("mltv-dodgers")){ele.innerHTML=
 let MLBwhitesox = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=whitesox">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - white sox | HD</li>
+<i class="us flag"></i> MLB TV - white sox | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-whitesox")){ele.innerHTML=(MLBwhitesox)};
@@ -33,7 +33,7 @@ for(const ele of document.getElementsByClassName("mltv-whitesox")){ele.innerHTML
 let MLBastros = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=astros">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - astros | HD</li>
+<i class="us flag"></i> MLB TV - astros | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-astros")){ele.innerHTML=(MLBastros)};
@@ -41,7 +41,7 @@ for(const ele of document.getElementsByClassName("mltv-astros")){ele.innerHTML=(
 let MLBbrewers = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=brewers">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - brewers | HD</li>
+<i class="us flag"></i> MLB TV - brewers | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-brewers")){ele.innerHTML=(MLBbrewers)};
@@ -49,7 +49,7 @@ for(const ele of document.getElementsByClassName("mltv-brewers")){ele.innerHTML=
 let MLBathletics = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=athletics">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - athletics | HD</li>
+<i class="us flag"></i> MLB TV - athletics | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-athletics")){ele.innerHTML=(MLBathletics)};
@@ -57,7 +57,7 @@ for(const ele of document.getElementsByClassName("mltv-athletics")){ele.innerHTM
 let MLBredsox = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=redsox">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - red sox | HD</li>
+<i class="us flag"></i> MLB TV - red sox | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-redsox")){ele.innerHTML=(MLBredsox)};
@@ -65,7 +65,7 @@ for(const ele of document.getElementsByClassName("mltv-redsox")){ele.innerHTML=(
 let MLBpadres = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=padres">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - padres | HD</li>
+<i class="us flag"></i> MLB TV - padres | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-padres")){ele.innerHTML=(MLBpadres)};
@@ -73,7 +73,7 @@ for(const ele of document.getElementsByClassName("mltv-padres")){ele.innerHTML=(
 let MLByankees = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=yankees">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - yankees | HD</li>
+<i class="us flag"></i> MLB TV - yankees | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-yankees")){ele.innerHTML=(MLByankees)};
@@ -81,7 +81,7 @@ for(const ele of document.getElementsByClassName("mltv-yankees")){ele.innerHTML=
 let MLBreds = `
 <a href="go:s6">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - reds | HD</li>
+<i class="us flag"></i> MLB TV - reds | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-reds")){ele.innerHTML=(MLBreds)};
@@ -89,7 +89,7 @@ for(const ele of document.getElementsByClassName("mltv-reds")){ele.innerHTML=(ML
 let MLBbluejays = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=bluejays">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - blue jays | HD</li>
+<i class="us flag"></i> MLB TV - blue jays | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-bluejays")){ele.innerHTML=(MLBbluejays)};
@@ -97,7 +97,7 @@ for(const ele of document.getElementsByClassName("mltv-bluejays")){ele.innerHTML
 let MLBphillies = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=phillies">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - phillies | HD</li>
+<i class="us flag"></i> MLB TV - phillies | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-phillies")){ele.innerHTML=(MLBphillies)};
@@ -105,7 +105,7 @@ for(const ele of document.getElementsByClassName("mltv-phillies")){ele.innerHTML
 let MLBmariners = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=mariners">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - mariners | HD</li>
+<i class="us flag"></i> MLB TV - mariners | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-mariners")){ele.innerHTML=(MLBmariners)};
@@ -113,7 +113,7 @@ for(const ele of document.getElementsByClassName("mltv-mariners")){ele.innerHTML
 let MLBbraves = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=braves">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - braves | HD</li>
+<i class="us flag"></i> MLB TV - braves | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-braves")){ele.innerHTML=(MLBbraves)};
@@ -121,7 +121,7 @@ for(const ele of document.getElementsByClassName("mltv-braves")){ele.innerHTML=(
 let MLBmets = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=mets">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - mets | HD</li>
+<i class="us flag"></i> MLB TV - mets | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-mets")){ele.innerHTML=(MLBmets)};
@@ -129,7 +129,7 @@ for(const ele of document.getElementsByClassName("mltv-mets")){ele.innerHTML=(ML
 let MLBangels = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=angels">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - angels | HD</li>
+<i class="us flag"></i> MLB TV - angels | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-angels")){ele.innerHTML=(MLBangels)};
@@ -137,7 +137,7 @@ for(const ele of document.getElementsByClassName("mltv-angels")){ele.innerHTML=(
 let MLBcardinals = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=cardinals">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - cardinals | HD</li>
+<i class="us flag"></i> MLB TV - cardinals | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-cardinals")){ele.innerHTML=(MLBcardinals)};
@@ -145,7 +145,7 @@ for(const ele of document.getElementsByClassName("mltv-cardinals")){ele.innerHTM
 let MLBindians = `
 <a href="go:s6">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - indians | HD</li>
+<i class="us flag"></i> MLB TV - indians | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-indians")){ele.innerHTML=(MLBindians)};
@@ -153,7 +153,7 @@ for(const ele of document.getElementsByClassName("mltv-indians")){ele.innerHTML=
 let MLBtigers = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=tigers">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - tigers | HD</li>
+<i class="us flag"></i> MLB TV - tigers | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-tigers")){ele.innerHTML=(MLBtigers)};
@@ -161,7 +161,7 @@ for(const ele of document.getElementsByClassName("mltv-tigers")){ele.innerHTML=(
 let MLBcubs = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=cubs">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - cubs | HD</li>
+<i class="us flag"></i> MLB TV - cubs | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-cubs")){ele.innerHTML=(MLBcubs)};
@@ -169,7 +169,7 @@ for(const ele of document.getElementsByClassName("mltv-cubs")){ele.innerHTML=(ML
 let MLBrockies = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=rockies">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - rockies | HD</li>
+<i class="us flag"></i> MLB TV - rockies | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-rockies")){ele.innerHTML=(MLBrockies)};
@@ -177,7 +177,7 @@ for(const ele of document.getElementsByClassName("mltv-rockies")){ele.innerHTML=
 let MLBnationals = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=nationals">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - nationals | HD</li>
+<i class="us flag"></i> MLB TV - nationals | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-nationals")){ele.innerHTML=(MLBnationals)};
@@ -185,7 +185,7 @@ for(const ele of document.getElementsByClassName("mltv-nationals")){ele.innerHTM
 let MLBroyals = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=royals">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - royals | HD</li>
+<i class="us flag"></i> MLB TV - royals | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-royals")){ele.innerHTML=(MLBroyals)};
@@ -193,7 +193,7 @@ for(const ele of document.getElementsByClassName("mltv-royals")){ele.innerHTML=(
 let MLBtwins = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=twins">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - twins | HD</li>
+<i class="us flag"></i> MLB TV - twins | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-twins")){ele.innerHTML=(MLBtwins)};
@@ -201,7 +201,7 @@ for(const ele of document.getElementsByClassName("mltv-twins")){ele.innerHTML=(M
 let MLBmarlins = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=marlins">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - marlins | HD</li>
+<i class="us flag"></i> MLB TV - marlins | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-marlins")){ele.innerHTML=(MLBmarlins)};
@@ -209,7 +209,7 @@ for(const ele of document.getElementsByClassName("mltv-marlins")){ele.innerHTML=
 let MLBpirates = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=pirates">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - pirates | HD</li>
+<i class="us flag"></i> MLB TV - pirates | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-pirates")){ele.innerHTML=(MLBpirates)};
@@ -217,7 +217,7 @@ for(const ele of document.getElementsByClassName("mltv-pirates")){ele.innerHTML=
 let MLBrangers = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=rangers">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - rangers | HD</li>
+<i class="us flag"></i> MLB TV - rangers | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-rangers")){ele.innerHTML=(MLBrangers)};
@@ -225,7 +225,7 @@ for(const ele of document.getElementsByClassName("mltv-rangers")){ele.innerHTML=
 let MLBorioles = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=orioles">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - orioles | HD</li>
+<i class="us flag"></i> MLB TV - orioles | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-orioles")){ele.innerHTML=(MLBorioles)};
@@ -233,7 +233,7 @@ for(const ele of document.getElementsByClassName("mltv-orioles")){ele.innerHTML=
 let MLBdiamondbacks = `
 <a href="http://slowdsports.000webhostapp.com/mlb.html?src=diamondbacks">
 <li class="canalito list-group-item darkolorbg blancolor">
-<i class="us flag"></i> MLB - diamondbacks | HD</li>
+<i class="us flag"></i> MLB TV - diamondbacks | HD</li>
 </a>
 `;
 for(const ele of document.getElementsByClassName("mltv-diamondbacks")){ele.innerHTML=(MLBdiamondbacks)};
