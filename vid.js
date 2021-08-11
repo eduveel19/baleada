8facaa5bdd632f014fff1ec56385a927a55c593d
Alex <slowdsports@gmail.com>
2021-08-11 15:11:35 -0600

    asdasd

--- vid.js
+++ vid.js
@@ -1,37 +1,3 @@
-//window.onload = function() {
-//             var player = new Clappr.Player({
-//                 source: srcLOAD,
-//                 parentId: '#player',
-//                 mimeType: 'audio/mpeg',
-//                 watermark: "https://i.ibb.co/kqmptsD/iRTVW.png", 
-//                 position: 'top-left',
-//                 plugins: [LevelSelector, ChromecastPlugin, ClapprPip.PipButton, ClapprPip.PipPlugin, DashShakaPlayback, Clappr.MediaControl, PlaybackRatePlugin],
-//                 events: {onReady: function() {var plugin = this.getPlugin('click_to_pause'); plugin && plugin.disable();},},
-//                 height: '100%',
-//                 width: '100%',
-//                 autoPlay: true,
-//                 mute: true,
-//                 shakaConfiguration: {
-//                 manifest: {retryParameters: {maxAttempts: Infinity}},
-//                 streaming: {retryParameters: {maxAttempts: Infinity}},
-//                 drm: {retryParameters: {maxAttempts: Infinity}},
-//                 },
-//                 playbackRateConfig: {
-//                 defaultValue: '1.00x',
-//                 options: [
-//                 {value: '0.10', label: '0.10x'},
-//                 {value: '0.25', label: '0.25x'},
-//                 {value: '0.50', label: '0.50x'},
-//                 {value: '0.75', label: '0.75x'},
-//                 {value: '1.00', label: '1.00x'},
-//                 {value: '1.25', label: '1.25x'},
-//                 {value: '1.50', label: '1.50x'},
-//                 {value: '1.75', label: '1.75x'},
-//                 {value: '2.00', label: '2.00x'},
-//                 ]
-//                 },
-//             });
-//         };
 // E P L
 let p1 = "http://161.35.58.89/live/ovochannel2/playlist.m3u8";
 let p2 = "https://sportsgrid-klowdtv.amagi.tv/playlist.m3u8";
